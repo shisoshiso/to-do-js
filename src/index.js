6bfddf566dc6bcb52750eef4007153b0378e99b5
Soshi Kono <k.soshi1219@gmail.com>
2021-11-28 11:04:02 +0900

    タスク追加機能（li,div,pタグ） li,div,pタグを追加した

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得、初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // p作成
+  const p = document.createElement("p");
+  p.className = "list-name";
+  p.innerText = inputText;
+
+  // liの子要素に各要素を設定
+  div.appendChild(p);
+  li.appendChild(div);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());

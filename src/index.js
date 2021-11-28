0a08fa0c4da26e4035f6fd20e89cce5b0b85f588
Soshi Kono <k.soshi1219@gmail.com>
2021-11-28 11:59:15 +0900

    タスク追加機能(ボタン) button 追加したボタンはアラートがなる設定。

--- src/index.js
+++ src/index.js
@@ -17,8 +17,24 @@ const onClickAdd = () => {
   p.className = "list-name";
   p.innerText = inputText;
 
+  // button(完了)作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // liの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   // 未完了のリストに追加
